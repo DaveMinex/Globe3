54b5807207a4206b9a4a4a184d90d9bec76bc5c1
PierreFabre <dave.minex@gmail.com>
2025-06-04 22:23:04 +0900

    Tiltipbox update

--- src/components/Dashboard/TooltipBox.tsx
+++ src/components/Dashboard/TooltipBox.tsx
@@ -17,8 +17,8 @@ export const TooltipBox: React.FC<TooltipBoxProps> = ({
     className={`absolute bg-white rounded-[18.44px] shadow-lg px-4 py-2 flex flex-col items-center  justify-center w-[183px] h-[98px] ${className}`}
     style={style}
   >
-    <div className="text-sfpro font-semibold text-nowrap font-[22.74px] " style={{ letterSpacing: '-0.03em' }}>{city}</div>
-    <div className="text-sfpro font-semibold text-nowrap font-[19.89px]" style={{ letterSpacing: '-0.03em' }}>{typeof users === 'number' ? users.toLocaleString() : users}</div>
+    <div className="text-sfpro font-semibold text-nowrap text-[22.74px] text-black " style={{ letterSpacing: '-0.03em' }}>{city}</div>
+    <div className="text-sfpro font-semibold text-nowrap text-[19.89px] text-black" style={{ letterSpacing: '-0.03em' }}>{typeof users === 'number' ? users.toLocaleString() : users}</div>
     <div className="text-sfpro font-medium font-[17.05px] text-[#0A1844]/50" style={{ letterSpacing: '-0.03em' }}>Active Users</div>
   </div>
 ); 
