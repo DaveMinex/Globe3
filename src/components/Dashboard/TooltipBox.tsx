976b64361cb8cc97d9f38f459bd0bca73544fc35
PierreFabre <dave.minex@gmail.com>
2025-06-04 22:46:10 +0900

    dashboard/tooltip darkmode

--- src/components/Dashboard/TooltipBox.tsx
+++ src/components/Dashboard/TooltipBox.tsx
@@ -12,13 +12,16 @@ export const TooltipBox: React.FC<TooltipBoxProps> = ({
   users,
   className = "",
   style = {},
-}) => (
-  <div
-    className={`absolute bg-white rounded-[18.44px] shadow-lg px-4 py-2 flex flex-col items-center  justify-center w-[183px] h-[98px] ${className}`}
-    style={style}
-  >
-    <div className="text-sfpro font-semibold text-nowrap text-[22.74px] text-black " style={{ letterSpacing: '-0.03em' }}>{city}</div>
-    <div className="text-sfpro font-semibold text-nowrap text-[19.89px] text-black" style={{ letterSpacing: '-0.03em' }}>{typeof users === 'number' ? users.toLocaleString() : users}</div>
-    <div className="text-sfpro font-medium font-[17.05px] text-[#0A1844]/50" style={{ letterSpacing: '-0.03em' }}>Active Users</div>
-  </div>
-); 
+}) => {
+
+  return (
+    <div
+      className={`absolute bg-white dark:bg-[#232323] rounded-[18.44px] shadow-lg px-4 py-2 flex flex-col items-center  justify-center w-[183px] h-[98px] ${className}`}
+      style={style}
+    >
+      <div className="text-sfpro font-semibold text-nowrap text-[22.74px] text-black dark:text-white" style={{ letterSpacing: '-0.03em' }}>{city}</div>
+      <div className="text-sfpro font-semibold text-nowrap text-[19.89px] text-black dark:text-[#0071E3]" style={{ letterSpacing: '-0.03em' }}>{typeof users === 'number' ? users.toLocaleString() : users}</div>
+      <div className="text-sfpro font-medium font-[17.05px] text-[#0A1844]/50 dark:text-[#0071E3]/50" style={{ letterSpacing: '-0.03em' }}>Active Users</div>
+    </div>
+  );
+}
